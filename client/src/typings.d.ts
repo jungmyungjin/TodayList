6ac2bc1b775643245f208c456f0a692b603c0a7a
jungmyungjin <myung2667@gmail.com>
2023-07-23 23:30:51 +0900

    fest : .svg, .module.scss 관련 ts 선언 추가

---
+++ client/src/typings.d.ts
@@ -0,0 +1,8 @@
+declare module '*.module.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
