aa6410345110fb03c00d763965f6303ac1d02ccd
jungmyungjin <myung2667@gmail.com>
2023-07-31 08:03:10 +0900

    feat : 회원가입 시 유효성 검증 구현

--- client/src/components/SignUp/SignUp.tsx
+++ client/src/components/SignUp/SignUp.tsx
@@ -3,13 +3,28 @@ import { useNavigate } from 'react-router-dom';
 import styles from './SignUp.module.scss';
 import keyboardIcon from '../../assets/icons/LoginKeyboard.svg';
 import userIcon from '../../assets/icons/LoginUser.svg';
+import { validateSinUp } from '../../services/authService.ts/signUpService';
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const [signUpReason, setSignUpReason] = useState('');
+  const [signUpErrorReason, setSignUpErrorReason] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const onClickGoBack = () => {
     navigate(-1);
+  };
+
+  const handleSubmitLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const validateErrorReason = validateSinUp(email, password, confirmPassword);
+
+    if (validateErrorReason === '') {
+      console.log('login 완료');
+    } else {
+      setSignUpErrorReason(validateErrorReason);
+    }
   };
 
   return (
@@ -17,26 +32,49 @@ const SignUp = () => {
       <div className={styles.LoginLayout}>
         <div className={styles.LoginTitle}>회원가입</div>
         <div className={styles.LoginFormLayout}>
-          {signUpReason === '' ? (
+          {signUpErrorReason === '' ? (
             ''
           ) : (
-            <span className={styles.SignUpErrorReason}>{signUpReason}</span>
+            <span key={signUpErrorReason} className={styles.SignUpErrorReason}>
+              {signUpErrorReason}
+            </span>
           )}
-          <form className={styles.LoginForm}>
+          <form onSubmit={handleSubmitLogin} className={styles.LoginForm}>
             <div className={styles.LoginInputLayout}>
               <div className={styles.LoginInput}>
                 <img src={userIcon} alt="" />
-                <input type="text" placeholder="이메일" autoFocus />
+                <input
+                  id="email"
+                  type="text"
+                  placeholder="이메일"
+                  autoFocus
+                  value={email}
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
               <div className={styles.LoginInput}>
                 <img src={keyboardIcon} alt="" />
-                {/* 눈모양 추가 */}
-                <input type="password" placeholder="비밀번호" />
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="문자, 숫자, 특수문자 포함 8자 이상"
+                  value={password}
+                  required
+                  title="비밀번호 문자, 숫자, 특수문자 포함 8자 이상"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
               <div className={styles.LoginInput}>
                 <img src={keyboardIcon} alt="" />
-                {/* 눈모양 추가 */}
-                <input type="password" placeholder="비밀번호 확인" />
+                <input
+                  id="confirmPassword"
+                  type="password"
+                  placeholder="비밀번호 확인"
+                  value={confirmPassword}
+                  required
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
               </div>
             </div>
             <div className={styles.RouteButtons}>
@@ -47,7 +85,7 @@ const SignUp = () => {
               >
                 이전
               </button>
-              <button type="submit" className={styles.GtHubLogin}>
+              <button type="submit" className={styles.SignUpLogin}>
                 가입하기
               </button>
             </div>
---
+++ client/src/services/authService.ts/signUpService.ts
@@ -0,0 +1,31 @@
+import {
+  validateEmail,
+  validatePassword,
+} from '../../utils/validateCredentials';
+
+/**
+ * 회원 가입, 유효성 검증 후 에러메세지 반환
+ * @param email
+ * @param password
+ * @param confirmPassword
+ * @returns string
+ */
+export const validateSinUp = (
+  email: string,
+  password: string,
+  confirmPassword: string
+): string => {
+  if (!validateEmail(email)) {
+    return '이메일 형식이 맞지 않습니다.';
+  }
+  if (!validatePassword(password)) {
+    return '패스워드 형식이 맞지 않습니다.';
+  }
+  if (password !== confirmPassword) {
+    return '비밀번호가 일치하지 않습니다.';
+  }
+  if (email === '이메일 존재하는지 확인') {
+    return '이미 사용중인 이메일 입니다.';
+  }
+  return '';
+};
