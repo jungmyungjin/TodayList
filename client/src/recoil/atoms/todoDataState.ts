d89d1e49dab9154e861a570dacaabb971bed5b8b
jungmyungjin <myung2667@gmail.com>
2023-08-03 19:03:51 +0900

    Updates

--- client/src/recoil/atoms/todoDataState.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil';
-import { TodoItem } from 'types/TodoList';
-
-export const TodoDataState = atom<TodoItem[]>({
-  key: 'todoDataState', // unique ID (with respect to other atoms/selectors)
-  default: [], // default value (aka initial value)
-});
--- client/src/typings.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.module.scss' {
-  const content: { [className: string]: string };
-  export default content;
-}
-declare module '*.svg' {
-  const content: any;
-  export default content;
-}
