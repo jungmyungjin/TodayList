7f7c0afa29559b74b0d385f82b280dbabf21ac9a
jungmyungjin <myung2667@gmail.com>
2023-08-08 12:30:15 +0900

    init : bcrypt 시스템 컴파일 의존성 개선을 위해 bcryptjs 패키지로 변경

--- server/src/api/services/userService.js
+++ server/src/api/services/userService.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { User } = require("../../models/index");
 const { generateAccessToken, passwordHash } = require("./authService");
 const { ConflictError, UnauthorizedError } = require("../../errors");
