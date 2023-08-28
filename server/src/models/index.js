79f65f3b54dbb5263e1bf2693e086a75d80dd2b6
jungmyungjin <myung2667@gmail.com>
2023-08-28 20:05:16 +0900

    fix : DB연결 실패시, 재시도 로직 추가
    #20

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -7,15 +7,28 @@ const Task = require("./task");
 
 Task.belongsTo(User, { as: "User", foreignKey: "user_email" });
 
-sequelize
-  .sync()
-  .then(() => {
+const MAX_RETRIES = 5;
+const RETRY_DELAY = 5000; // 5 seconds
+
+const connectWithRetry = async (retries = 0) => {
+  if (retries > MAX_RETRIES) {
+    console.error("Max retries reached. Exiting...");
+    process.exit(1);
+  }
+
+  try {
+    await sequelize.sync();
     console.log("------------------------------");
     console.log("💫 Database & tables created!");
     console.log("------------------------------");
-  })
-  .catch((error) => {
-    console.error("Failed to create database & tables:", error);
-  });
+  } catch (error) {
+    console.error(
+      `Failed to create database & tables. Retry attempt: ${retries + 1}`
+    );
+    setTimeout(() => connectWithRetry(retries + 1), RETRY_DELAY);
+  }
+};
 
-module.exports = { sequelize, User, Task };
+connectWithRetry();
+
+module.exports = { User, Task };
